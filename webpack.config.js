69e028e879f5eff6ce5b093f884e2bfb30da90ea
Hsieh Ching Hsiang <vita_hsieh@morrisonexpress.com>
2021-02-01 08:39:16 +0800

    add about react package and file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist/'),
@@ -9,11 +9,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.(js|jsx)$/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
           },
         },
       },
